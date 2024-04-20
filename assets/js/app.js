bde6367c698a547958a050f1749ab6dc8e08507c
dh-pardeep <pardeepjangra514@gmil.com>
2024-04-20 09:52:55 +0530

    slider done

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,3 +57,55 @@ window.addEventListener("load", (event) => {
     }
   }
 });
+
+// slider hero
+$(".hero_slider").slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 0,
+  arrows: false, 
+  dots: false,
+  infinite: true,
+  speed: 8000,
+  pauseOnHover: false,
+  cssEase: "linear",
+  responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        slidesToShow: 5,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2.3,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1.8,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1.2,
+      },
+    },
+  ],
+});
