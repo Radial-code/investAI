c45578efa7a8c38333db2d7d77db979ea4a5f199
dh-pardeep <pardeepjangra514@gmil.com>
2024-04-22 12:40:12 +0530

    timer js link remove and extra code remove

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,56 +8,6 @@ function toggleNavigationMenu() {
   changeButton.classList.toggle("change");
 }
 
-//timer counter
-window.addEventListener("load", (event) => {
-  let clock;
-
-  // Grab the current date
-  let currentDate = new Date();
-
-  // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2024-04-25 23:59", "New Delhi/India");
-
-  // Calculate the difference in seconds between the future and current date
-  let diff = targetDate / 1000 - currentDate.getTime() / 1000;
-
-  if (diff <= 0) {
-    // If remaining countdown is 0
-    clock = ".clock".FlipClock(0, {
-      clockFace: "DailyCounter",
-      countdown: true,
-      autostart: false,
-    });
-    console.log("Date has already passed!");
-  } else {
-    // Run countdown timer
-    clock = $(".clock").FlipClock(diff, {
-      clockFace: "DailyCounter",
-      countdown: true,
-      callbacks: {
-        stop: function () {
-          console.log("Timer has ended!");
-        },
-      },
-    });
-
-    // Check when timer reaches 0, then stop at 0
-    setTimeout(function () {
-      checktime();
-    }, 1000);
-
-    function checktime() {
-      t = clock.getTime();
-      if (t <= 0) {
-        clock.setTime(0);
-      }
-      setTimeout(function () {
-        checktime();
-      }, 1000);
-    }
-  }
-});
-
 // slider hero
 $(".hero_slider").slick({
   slidesToShow: 6,
@@ -297,3 +247,26 @@ function isValidPhoneNumber(phoneNumber) {
   const phoneRegex = /^[0-9]{10}$/;
   return phoneRegex.test(phoneNumber);
 }
+
+// timer
+function CountDownTimer() {
+  left_days = document.getElementById("days");
+  left_hours = document.getElementById("hours");
+  left_minutes = document.getElementById("minutes");
+  left_seconds = document.getElementById("seconds");
+}
+function showRemaining() {
+  now = new Date();
+
+  days = (6 - now.getDay()).toString().padStart(2, "0");
+  hours = (23 - now.getHours()).toString().padStart(2, "0");
+  minutes = (59 - now.getMinutes()).toString().padStart(2, "0");
+  seconds = (59 - now.getSeconds()).toString().padStart(2, "0");
+  left_days.innerHTML = days;
+  left_hours.innerHTML = hours;
+  left_minutes.innerHTML = minutes;
+  left_seconds.innerHTML = seconds;
+}
+
+timer = setInterval(showRemaining, 1000);
+CountDownTimer();
